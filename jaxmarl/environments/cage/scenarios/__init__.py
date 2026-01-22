7f4f7a7930cfbd230a3f25608400edb1210f0573
Paul Elliott <paul@vizworkshop.com>
2026-01-22 18:15:02 -0500

    Add CAGE-JAX scenario configuration

---
+++ jaxmarl/environments/cage/scenarios/__init__.py
@@ -0,0 +1,15 @@
+"""CAGE scenario configurations."""
+
+from jaxmarl.environments.cage.scenarios.scenario2 import (
+    CONST,
+    SCENARIO_DESCRIPTION,
+    HOST_CONFIG,
+    get_scenario_const,
+)
+
+__all__ = [
+    'CONST',
+    'SCENARIO_DESCRIPTION',
+    'HOST_CONFIG',
+    'get_scenario_const',
+]
